06e63ac10a633a138f64ff6612f60d85e842b3da
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2016-01-08 21:40:36 +0000

    add error handling when no offtarget found
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GUIDEseq@112363 bc3139a8-67e5-0310-9ffc-ced21a209358
    
    Former-commit-id: 14d68e15ebc413c8026b6e28f93a807f01581f39 [formerly 5141a243cc99c439447de9be4976d8393df5eec9]
    Former-commit-id: a850f26888579d727c438202fb0dfc69f6c976db

--- R/offTargetAnalysisOfPeakRegions.R
+++ R/offTargetAnalysisOfPeakRegions.R
@@ -36,12 +36,19 @@ offTargetAnalysisOfPeakRegions <-
         stop("only bed file with at least 4 columns are supported")
     }
     thePeaks[, 4] <- gsub(" ", "", thePeaks[,4])
-    TS2 <- compare2Sequences(inputFile1Path = gRNA, inputFile2Path = peaks,
+    TS2 <- tryCatch(
+        (compare2Sequences(inputFile1Path = gRNA, inputFile2Path = peaks,
         findgRNAsWithREcutOnly = FALSE, format = format,
         header = peaks.withHeader,
-        BSgenomeName = BSgenomeName, upstream = upstream,
+        BSgenomeName = BSgenomeName,
+        upstream = upstream,
         downstream = downstream,
-        minREpatternSize = 4, searchDirection = "1to2",
+        minREpatternSize = 4,
+        findgRNAs = c(FALSE, FALSE),
+        removegRNADetails = c(TRUE, FALSE),
+        exportAllgRNAs =  "no",
+        annotatePaired = FALSE,
+        searchDirection = "1to2",
         overlap.gRNA.positions = overlap.gRNA.positions,
         findPairedgRNAOnly = FALSE,
         min.gap = 0, max.gap = 20, gRNA.name.prefix = "gRNA",
@@ -50,61 +57,72 @@ offTargetAnalysisOfPeakRegions <-
         max.mismatch = max.mismatch,
         outputDir = outputDir, foldgRNAs = FALSE,
         allowed.mismatch.PAM = allowed.mismatch.PAM, overwrite = overwrite,
-        weights = weights
-    )
-    n.cores <- detectCores() - 1
-    cl <- makeCluster(n.cores)
+        weights = weights)),
+        error = function(e) { print(e); "error" })
+
+    if ( TS2 != "error")
+    {
+        n.cores <- detectCores() - 1
+        cl <- makeCluster(n.cores)
    
-   names <- as.character(unlist(parLapply(cl, as.character(TS2$offTarget),
-       function(temp) {
+        names <- as.character(unlist(parLapply(cl, as.character(TS2$offTarget),
+           function(temp) {
            temp1 <- strsplit(temp,":")[[1]]
            end.ind <- length(temp1) - 1
            paste(temp1[1:end.ind], collapse=":")
-    })))
-    TS2 <- cbind(names = names, TS2)
-    excluding.columns = which(colnames(TS2) %in% 
-        c("scoreForSeq1", "targetInSeq1", "gRNAefficacy", "scoreDiff"))
-    TS2 <- TS2[, -excluding.columns]
-    colnames(TS2)[colnames(TS2) == "scoreForSeq2"] = "predicted_cleavage_score"
-    colnames(TS2)[colnames(TS2) == "targetInSeq2"] = "offTarget_sequence"
-    offTargetOffset <- do.call(rbind, parLapply(cl, as.character(TS2$offTarget),
-        function(temp) 
-        { 
+        })))
+        TS2 <- cbind(names = names, TS2)
+        excluding.columns = which(colnames(TS2) %in% 
+            c("scoreForSeq1", "targetInSeq1", "gRNAefficacy", "scoreDiff"))
+        TS2 <- TS2[, -excluding.columns]
+        colnames(TS2)[colnames(TS2) == "scoreForSeq2"] = "predicted_cleavage_score"
+        colnames(TS2)[colnames(TS2) == "targetInSeq2"] = "offTarget_sequence"
+        offTargetOffset <- do.call(rbind, parLapply(cl, as.character(TS2$offTarget),
+           function(temp) 
+           { 
             temp1 <- strsplit(temp,":")[[1]]
             start.ind <- length(temp1)
             as.numeric(strsplit(temp1[start.ind], "-")[[1]][1:2])
         }))
-    stopCluster(cl)
-    TS2 <- cbind(TS2, offTarget_Start = offTargetOffset[,1], 
-        offTarget_End = offTargetOffset[,2])
-    offtargets <- merge(TS2, thePeaks, by = "names", all = TRUE)
-    offtargets$offTarget_Start <- 
-        ifelse(as.character(offtargets$peak_strand) == "+",
-        offtargets$offTarget_Start - upstream + offtargets$peak_start - 1,
-        upstream - offtargets$offTarget_End + offtargets$peak_end + 1)
-    offtargets$offTarget_End <- 
-        offtargets$offTarget_Start + PAM.size  + gRNA.size  - 1
+        stopCluster(cl)
+        TS2 <- cbind(TS2, offTarget_Start = offTargetOffset[,1], 
+           offTarget_End = offTargetOffset[,2])
+        offtargets <- merge(TS2, thePeaks, by = "names", all = TRUE)
+        offtargets$offTarget_Start <- 
+            ifelse(as.character(offtargets$peak_strand) == "+",
+            offtargets$offTarget_Start - upstream + offtargets$peak_start - 1,
+            upstream - offtargets$offTarget_End + offtargets$peak_end + 1)
+        offtargets$offTarget_End <- 
+            offtargets$offTarget_Start + PAM.size  + gRNA.size  - 1
    
-    offtargets.minus.minus <- 
-        subset(offtargets, as.character(offtargets$peak_strand) == "-" & 
-        as.character(offtargets$offTargetStrand) == "-")
-    offtargets.minus.plus <- subset(offtargets,
-        as.character(offtargets$peak_strand) == "-" & 
-        as.character(offtargets$offTargetStrand) == "+")
-    if (dim(offtargets.minus.minus)[1] > 0)
-        offtargets.minus.minus$offTargetStrand <- "+"
-    if (dim(offtargets.minus.plus)[1] > 0)
-        offtargets.minus.plus$offTargetStrand<- "-"
+        offtargets.minus.minus <- 
+            subset(offtargets, as.character(offtargets$peak_strand) == "-" & 
+            as.character(offtargets$offTargetStrand) == "-")
+        offtargets.minus.plus <- subset(offtargets,
+            as.character(offtargets$peak_strand) == "-" & 
+            as.character(offtargets$offTargetStrand) == "+")
+        if (dim(offtargets.minus.minus)[1] > 0)
+            offtargets.minus.minus$offTargetStrand <- "+"
+        if (dim(offtargets.minus.plus)[1] > 0)
+            offtargets.minus.plus$offTargetStrand<- "-"
    
-    offtargets <- rbind(subset(offtargets,
-        as.character(offtargets$peak_strand) == "+" | 
-        as.character(offtargets$peak_strand) == "*" | 
-        is.na(offtargets$offTargetStrand)),
-        offtargets.minus.minus, offtargets.minus.plus)
+        offtargets <- rbind(subset(offtargets,
+            as.character(offtargets$peak_strand) == "+" | 
+            as.character(offtargets$peak_strand) == "*" | 
+            is.na(offtargets$offTargetStrand)),
+            offtargets.minus.minus, offtargets.minus.plus)
    
-    write.table(offtargets, 
-        file = file.path(outputDir,"offTargetsInPeakRegions.tsv"),
-        sep="\t", row.names = FALSE)
-    unlink(file.path(outputDir, "scoresFor2InputSequences.xls"))
-    offtargets
+        write.table(offtargets, 
+            file = file.path(outputDir,"offTargetsInPeakRegions.xls"),
+            sep="\t", row.names = FALSE)
+        unlink(file.path(outputDir, "scoresFor2InputSequences.xls"))
+        offtargets
+     }
+    else
+    {
+        write.table(thePeaks,
+            file = file.path(outputDir,"offTargetsInPeakRegions.xls"),
+            sep="\t", row.names = FALSE)
+        thePeaks 
+    }
 }
