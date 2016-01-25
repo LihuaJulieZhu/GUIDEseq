38d1ddea7e79990262f46645ac0297172bcc707f
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2016-01-25 21:39:51 +0000

    added combineOfftargets function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GUIDEseq@112883 bc3139a8-67e5-0310-9ffc-ced21a209358
    
    Former-commit-id: 0a4127f330900bed36c9fdbb5f751ddc868a994a [formerly d467d6929755451f3fd3ee8116e777ddb721ddb9]
    Former-commit-id: 067290875d9c8ce8f55c6676b9eba7fb2210805d

---
+++ R/combineOfftargets.R
@@ -0,0 +1,39 @@
+combineOfftargets <- function(offtarget.folder, 
+    sample.name, 
+    offtarget.filename = "offTargetsInPeakRegions.xls",
+    common.col = c("targetSeqName", "chromosome", "offTargetStrand",
+        "offTarget_Start", "offTarget_End","gRNAPlusPAM", 
+        "offTarget_sequence", "n.mismatch",  
+        "guideAlignment2OffTarget","predicted_cleavage_score"),
+    exclude.col = 2,
+    outputFileName)
+{
+    all <- read.table(file.path(offtarget.folder[1], 
+        offtarget.filename, fsep = .Platform$file.sep),
+        sep="\t", header = TRUE)
+    all <- subset(all, !is.na(all$offTarget))
+    all <- all[, -exclude.col]
+    if(length(setdiff(common.col, colnames(all))) > 0)
+    {
+        stop(paste(setdiff(common.col, colnames(all)), 
+            "are not valid column names! Please make sure to 
+            specify the correct column names for common.col!"))
+    }
+
+    colnames(all)[!colnames(all) %in% common.col] <- paste(sample.name[1], 
+        colnames(all)[!colnames(all) %in% common.col], sep=".")
+
+    for (i in 2:length(offtarget.folder))
+    {
+        off <- read.table(file.path(offtarget.folder[i], offtarget.filename,
+            fsep = .Platform$file.sep), sep="\t", header = TRUE)
+        off <- subset(off, !is.na(off$offTarget))
+        off <- off[,-exclude.col]
+        colnames(off)[!colnames(off) %in% common.col] <- paste(sample.name[i], 
+            colnames(off)[!colnames(off) %in% common.col], sep=".")
+
+        all <- merge(all, off, by = common.col, all = TRUE)
+    }
+    write.table(all, file = outputFileName, sep="\t", row.names=FALSE)
+    all
+}
